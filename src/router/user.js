fdc70a0f7d22d85233adb07ca045cbbc5a37e38b
Im-Siyoun <rlagusdn3321@naver.com>
2024-01-08 01:36:59 +0900

    2-3. 서버에서 데이터를 받아오는 두 가지 방법

--- src/router/user.js
+++ src/router/user.js
@@ -3,16 +3,23 @@ import { Router } from "express";
 const UserRouter = Router();
 
 UserRouter.post('/', (req, res) => {
-  res.send('POST 요청을 받았습니다.');
+  const { body } = req;
+  console.log(body.id, body.password);
+  res.json({
+    id: body.id,
+    password: body.password,
+  });
 });
 UserRouter.get('/', (req, res) => {
   res.send('GET 요청을 받았습니다.');
 });
-UserRouter.patch('/', (req, res) => {
-  res.send('PATCH 요청을 받았습니다.');
+UserRouter.patch('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`${id}의 수정 요청을 받았습니다.`);
 });
-UserRouter.delete('/', (req, res) => {
-  res.send('DELETE 요청을 받았습니다.');
+UserRouter.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`${id}의 삭제 요청을 받았습니다.`);
 });
 
 export default UserRouter;
