c515e68ab97a7a05d0f3eb396783f070aabf91b7
Im-Siyoun <rlagusdn3321@naver.com>
2024-01-09 23:31:17 +0900

    3-1. mongodb 클러스터와 연결

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import UserRouter from './router/user.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(UserRouter);
+
+mongoose.connect("mongodb+srv://rlatldbs3321:by192472@cluster0.evl85.mongodb.net/lecture?retryWrites=true&w=majority");
 
 app.listen(3000, () => {
   console.log('서버가 실행되었습니다.');
